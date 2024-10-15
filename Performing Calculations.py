434e052831dfb1a5a7d212a3aac5804a8dfd2069
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-10-15 13:30:59 +0400

    Update Performing Calculations.py

--- Performing Calculations.py
+++ Performing Calculations.py
@@ -32,3 +32,18 @@ a = 20
 b = 10
 sum = a // b
 print('the sum of two numbers is' ,sum)
+
+# result = var1 * 2 + var2 * 3 + \ var3 * 4 + var4 * 5
+
+# print("Monday's sales are", Monday, "and Tuesday's sales are", Tuesday,"and Wednesday's sales are", Wednesday)
+
+# total = (value1 + value2 + value3 + value4 + value5 + value6)
+
+message = 'Hello ' + 'Hatsune' + ' Miku!'
+print(message)
+
+print('Enter the amount of ' +
+      'sales for each day and ' +
+      'press Enter.')
+
+
