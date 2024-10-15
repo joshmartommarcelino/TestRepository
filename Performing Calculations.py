5e531eaa23888a95f7938b6a21b847522e6a7a8f
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-10-15 13:13:37 +0400

    Performing Calulations.py
    +,-,*,/,//,%,**

---
+++ Performing Calculations.py
@@ -0,0 +1,34 @@
+a = 20
+b = 10
+sum = a + b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a - b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a / b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a * b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a % b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a ** b
+print('the sum of two numbers is' ,sum)
+
+a = 20
+b = 10
+sum = a // b
+print('the sum of two numbers is' ,sum)
--- print.py
+++ print.py
@@ -3,3 +3,12 @@ print('is')
 print('fun!')
 
 variable = input ('What is you Good Name?')
+
+name = input('What is your name?')
+
+function1(function2(argument))
+
+int(item)
+float(item)
+
+score = float(input('Enter your score:'))
