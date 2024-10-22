6e7d9abb6171c8182cec1cd1b8ef065f29595316
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-10-22 12:55:21 +0400

    Update Performing Calculations.py

--- Performing Calculations.py
+++ Performing Calculations.py
@@ -91,8 +91,8 @@ turtle.circle (100)
 
 age = input ("Enter your age")
 
-#if(age = 18)
-#print("You are an adult")
-#else
-#print("you are child")
-
+age = input ("enter your age")
+if age > 18:
+      print("You are a minor")
+elif age < 18:
+      print("you are an adult")
