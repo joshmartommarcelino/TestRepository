591076a9959ff8ed31c1d5bf66cbb29bc524953b
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-10-26 20:38:51 +0400

    Update 10% discount.py

--- 10% discount.py
+++ 10% discount.py
@@ -24,3 +24,16 @@ else:
 
 # Bank Withdrawal(Nested if Else)
 
+account_balance = float(input("Enter account balance: "))
+withdraw_account = float(input("Enter the amount you want to witdraw: "))
+
+if withdraw_account <= account_balance:
+    new_balance = account_balance - withdraw_account
+    print(f"Withdrawal succesful. Your new balance is: £{new_balance}")
+    if  new_balance > 1000:
+        print("\033[1mHello esteemed client!\033[0m")
+    elif new_balance < 100:
+        print(f"Warning: Your £{new_balance} is below £100.")
+else:
+    print(f"Insufficient funds. Withdrawal not allowed.")
+
