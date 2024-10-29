54dec09ef26b14876430c98cf188307b778ce69a
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-10-29 13:33:35 +0400

    lists

---
+++ # lists are compiled pieces of info.py
@@ -0,0 +1,18 @@
+# lists are compiled pieces of info
+
+integers = [2, 4, 6, 8, 10]
+print(integers)
+
+strings = ["Josh", "Allen", "Jheirom", "Denson", "Aniel"]
+print(strings)
+
+float = [ "Miku", 39, 1.01]
+print(float)
+
+marks=[2,45,67,89,25]
+
+print(marks[-2])
+
+print(len(marks))
+
+print(marks + float)
--- print.py
+++ print.py
@@ -6,3 +6,28 @@ variable = input ('What is you Good Name?')
 
 num = 12345.6789
 print(f'The number is {num:12,.2f}')
+
+# Loop using i as internal variable
+for i in range(3):
+  print(i)
+
+print("--")   #This is just a separator
+
+#Loop using "something" as internal variable
+for something in range(3):
+  print(something)
+
+integers = [2, 4, 6, 8, 10]
+print(integers)
+
+strings = ["Josh", "Allen", "Jheirom", "Denson", "Aniel"]
+print(strings)
+
+float = [ "Miku", 39, 1.01]
+print(float)
+
+marks=[2,45,67,89,25]
+
+print(marks[-2])
+
+print(len(marks))
