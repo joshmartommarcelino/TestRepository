0dc27cd3603a187b0915782136c7e2712eb99db8
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-05 13:44:10 +0400

    Update colortuples.py

--- colortuples.py
+++ colortuples.py
@@ -8,3 +8,19 @@ numbers1 = (1,2,3)
 numbers2 = (4,5,6)
 numbers_combined = numbers1 + numbers2
 print(numbers_combined)
+
+alphabet = ('a','b','c','d','e','f','g')
+print(alphabet[:3])
+print(alphabet[-3])
+print(alphabet[::2])
+
+numbers = (1,2,2,3,4,4,4,5)
+print(numbers.count(4))
+print(numbers.index(2))
+
+nested = (1,2,(3,4),5)
+print(nested[2][1])
+
+colors = ('red','green','blue')
+print('yellow' in colors)
+print('blue' in colors)
