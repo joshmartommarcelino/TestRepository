20c409fa24d7eb90c60b529a9c6e0e6fcce26f3c
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-05 13:01:28 +0400

    tuples 1

---
+++ colortuples.py
@@ -0,0 +1,5 @@
+colors = ('red', 'green', 'blue')
+color1, color2, color3 = colors
+print(color1)
+print(color2)
+print(color3)
---
+++ tuples.py
@@ -0,0 +1,8 @@
+fruits = ('apple', 'banana', 'cherry')
+print(fruits)
+#error on purpose(you cannot change tuples)
+fruits[0] = 'orange'
+
+print(fruits[1])
+print(fruits[-1])
+
