eae9b7928d55be074926ee1566d37e7ac4b737f1
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-06 14:29:20 +0400

    Create dictionary excercises.py

---
+++ dictionary excercises.py
@@ -0,0 +1,24 @@
+student = {
+    'name' : 'Alice',
+    'age' : 20,
+    'courses' : ['Math', 'Physics']
+}
+print(student)
+
+Product = {
+    'Title' : 'Guitar',
+    'price' : 250,
+    'Quantity' :1,
+}
+print(Product)
+
+student_info = {
+    'name' : 'Bob',
+    'age' : 21,
+    'major' : 'Computer Science', 
+}
+
+student_info_2 = dict(name ="Emma",age=22,major="Biology")
+
+print(student_info)
+print(student_info_2)
