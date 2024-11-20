76367d7bda6a681f3f5dfb6caa45367288198279
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-20 14:52:58 +0400

    Create passing aranggments to functions.py

---
+++ passing aranggments to functions.py
@@ -0,0 +1,25 @@
+# this function multiplies the number by 2
+
+def show_double(number):
+    result = number * 2
+    print(result)
+
+def main():
+    value = int(input("Enter number: "))
+    show_double(value)
+
+if __name__ == "__main__":
+    main()
+
+#this function prints the number 2x
+
+def show_double2(number):
+    result = number * 2
+    print(result)
+
+def main():
+    value = 5
+    show_double2(value)
+
+if __name__ == "__main__":
+    main()
