d5e6fbc522ded488d1290148a505117fb5fd0426
Josh Martom Marcelino <johnsh2508@gmail.com>
2024-11-20 14:37:30 +0400

    names

--- import turtle.py
+++ import turtle.py
@@ -1,3 +1,13 @@
 import turtle
+turtle.forward(100)
+turtle.left(100)
 turtle.circle(100)
+turtle.forward(100)
+turtle.right(100)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
 turtle.mainloop()
+
